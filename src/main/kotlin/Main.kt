f5ff0e39a15cfbf6eaea528a0bdbabc28ac8fa4a
HusseinElFeky <hussein.feky@gmail.com>
2021-12-07 23:36:42 +0200

    Change window background to a light-colored image

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -6,10 +9,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.type
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
@@ -38,19 +44,26 @@ fun main() = application {
         undecorated = true
     ) {
         MaterialTheme {
-            when (screenState) {
-                ScreenState.MainMenu -> {
-                    Button(onClick = {
-                        text = "Hello, Desktop!"
-                    }) {
-                        Text(text)
+            Box(modifier = Modifier.fillMaxSize()) {
+                Image(
+                    painter = painterResource("images/bg_default.jpg"),
+                    contentDescription = "Window Background",
+                    contentScale = ContentScale.FillBounds
+                )
+                when (screenState) {
+                    ScreenState.MainMenu -> {
+                        Button(onClick = {
+                            text = "Hello, Desktop!"
+                        }) {
+                            Text(text)
+                        }
                     }
-                }
-                is ScreenState.TypingTest -> {
+                    is ScreenState.TypingTest -> {
 
-                }
-                is ScreenState.TestResult -> {
+                    }
+                    is ScreenState.TestResult -> {
 
+                    }
                 }
             }
         }
