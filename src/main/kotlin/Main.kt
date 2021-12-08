8d002447997d81b52a1b1739c82900cddb76d858
HusseinElFeky <hussein.feky@gmail.com>
2021-12-08 03:06:13 +0200

    Implement main menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,27 @@
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.getValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.type
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
@@ -25,10 +31,8 @@ import androidx.compose.ui.window.rememberWindowState
 fun main() = application {
 
     val windowState = rememberWindowState(placement = WindowPlacement.Fullscreen)
-    var screenState = remember<ScreenState> { ScreenState.MainMenu }
-    var keyboardLayout = remember { KeyboardLayout.QWERTY }
-
-    var text by remember { mutableStateOf("Hello, World!") }
+    val screenState = remember { mutableStateOf<ScreenState>(ScreenState.MainMenu) }
+    val keyboardLayout = remember { mutableStateOf(KeyboardLayout.QWERTY) }
 
     Window(
         state = windowState,
@@ -50,22 +54,86 @@ fun main() = application {
                     contentDescription = "Window Background",
                     contentScale = ContentScale.FillBounds
                 )
-                when (screenState) {
+                when (screenState.value) {
                     ScreenState.MainMenu -> {
-                        Button(onClick = {
-                            text = "Hello, Desktop!"
-                        }) {
-                            Text(text)
-                        }
+                        renderMainMenu(screenState, keyboardLayout)
                     }
                     is ScreenState.TypingTest -> {
-
+                        renderTypingTest(screenState, keyboardLayout)
                     }
                     is ScreenState.TestResult -> {
-
+                        renderTestResult(screenState, keyboardLayout)
                     }
                 }
             }
         }
     }
+}
+
+@Composable
+fun renderMainMenu(screenState: MutableState<ScreenState>, keyboardLayout: MutableState<KeyboardLayout>) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            modifier = Modifier.wrapContentHeight(),
+            text = "Keyboards \uD83E\uDD37\u200D♂️\uD83E\uDD37\u200D♂️",
+            textAlign = TextAlign.Center,
+            fontSize = 60.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
+        Row(
+            modifier = Modifier.wrapContentHeight(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            KeyboardLayout.values().forEach { layout ->
+                Row(
+                    modifier = Modifier.wrapContentWidth().padding(30.dp),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = layout == keyboardLayout.value,
+                        colors = RadioButtonDefaults.colors(selectedColor = Color(0xFF4C597D)),
+                        onClick = {
+                            keyboardLayout.value = layout
+                        }
+                    )
+                    ClickableText(
+                        text = AnnotatedString(layout.name),
+                        style = TextStyle(fontSize = 20.sp),
+                        onClick = {
+                            keyboardLayout.value = layout
+                        }
+                    )
+                }
+            }
+        }
+        Button(
+            modifier = Modifier.wrapContentHeight(),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF4C597D)),
+            onClick = {
+                screenState.value = ScreenState.TypingTest.create()
+            }
+        ) {
+            Text(
+                text = "Start Test",
+                color = Color.White,
+                fontSize = 40.sp
+            )
+        }
+    }
+}
+
+@Composable
+fun renderTypingTest(screenState: MutableState<ScreenState>, keyboardLayout: MutableState<KeyboardLayout>) {
+
+}
+
+@Composable
+fun renderTestResult(screenState: MutableState<ScreenState>, keyboardLayout: MutableState<KeyboardLayout>) {
+
 }
--- src/main/kotlin/ScreenState.kt
+++ src/main/kotlin/ScreenState.kt
@@ -7,7 +7,19 @@ sealed class ScreenState {
         currentIndex: Int,
         incorrectCharacters: Int,
         timeTaken: Int
-    ) : ScreenState()
+    ) : ScreenState() {
+
+        companion object {
+            fun create(): TypingTest {
+                return TypingTest(
+                    paragraph = "Technology refers to the practical application of scientific knowledge for a purpose. It also includes the capability and skills required to apply the knowledge. Technology enhances the usefulness of goods and services in a safe manner.",
+                    currentIndex = 0,
+                    incorrectCharacters = 0,
+                    timeTaken = 0
+                )
+            }
+        }
+    }
 
     class TestResult(
         timeTaken: Int,
